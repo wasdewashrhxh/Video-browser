cc87b0cc20452a91d0e578c96abc362ea45f1c5b
wasdewashrhxh <150166580+wasdewashrhxh@users.noreply.github.com>
2026-03-11 13:48:22 -0500

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,1 +1,11 @@
-// Intentionally minimal preload for future native integrations.
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('libraryApi', {
+  scan: () => ipcRenderer.invoke('library:scan'),
+  importMedia: (targetFolderId) => ipcRenderer.invoke('library:import-media', targetFolderId),
+  createFolder: (name) => ipcRenderer.invoke('library:create-folder', name),
+  deleteFolder: (folderId) => ipcRenderer.invoke('library:delete-folder', folderId),
+  deleteMedia: (mediaPath) => ipcRenderer.invoke('library:delete-media', mediaPath),
+  moveMedia: (mediaPath, targetFolderId) => ipcRenderer.invoke('library:move-media', mediaPath, targetFolderId),
+  openLibraryFolder: () => ipcRenderer.invoke('library:open-folder'),
+});
